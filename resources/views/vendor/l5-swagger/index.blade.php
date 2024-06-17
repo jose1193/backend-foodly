472d2170921020989269eaedc5d18b8b31aa0af2
jose1193 <josegonzalezcr2794@gmail.com>
2024-06-17 18:52:38 +0100

    Add: added docker composer

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <title>Foodly APIs</title>
     <meta name="theme-color" content="#79005d" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('swagger/swagger-ui.min.css') }}">
-    <link rel="icon" type="image/png" href="{{ asset('favicon/favicon-32x32.png') }}" sizes="32x32" />
-    <link rel="icon" type="image/png" href="{{ asset('favicon/favicon-16x16.png') }}" sizes="16x16" />
+    <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/swagger/swagger-ui.min.css">
+    <link rel="icon" type="image/png" href="{{ env('APP_URL') }}/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ env('APP_URL') }}/favicon/favicon-16x16.png" sizes="16x16" />
 
 
     <style>
@@ -86,9 +86,8 @@
 
 
 
-
     <script src="{{ l5_swagger_asset($documentation, 'swagger-ui-bundle.js') }}"></script>
-    <script src="{{ asset('swagger/swagger-ui-standalone-preset.js') }}"></script>
+    <script src="{{ env('APP_URL') }}/swagger/swagger-ui-standalone-preset.js"></script>
     <script>
         window.onload = function() {
             // Build a system
